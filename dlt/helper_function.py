90d852f189317637bfd54d022301871b065bf80f
maiqkhan <33614138+maiqkhan@users.noreply.github.com>
2025-07-11 01:23:47 +0000

    use dlt to embed data in qdrant

---
+++ dlt/helper_function.py
@@ -0,0 +1,33 @@
+import requests 
+import dlt 
+
+from dlt.destinations import qdrant
+
+@dlt.resource(write_disposition='replace', name='zoomcamp_data')
+def zoomcamp_data():
+    docs_url = 'https://github.com/alexeygrigorev/llm-rag-workshop/raw/main/notebooks/documents.json'
+    docs_response = requests.get(docs_url)
+    documents_raw = docs_response.json()
+
+    for course in documents_raw:
+        course_name = course['course']
+
+        for doc in course['documents']:
+            doc['course'] = course_name 
+            yield doc
+
+
+qdrant_destination = qdrant(
+    qd_path = "db.qdrant"
+)
+
+
+pipeline = dlt.pipeline(
+    pipeline_name = "zoomcamp_pipeline",
+    destination = qdrant_destination,
+    dataset_name = "zoomcamp_tagged_data"
+)
+
+load_info = pipeline.run(zoomcamp_data())
+
+print(pipeline.last_trace)
